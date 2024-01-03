b49b5e0e9f225d16ef137c0cfb035c95d1d15e91
dev-status-200 <dev.status.200@gmail.com>
2024-01-03 22:50:04 +0500

    update

--- routes/appointments/index.js
+++ routes/appointments/index.js
@@ -10,7 +10,4 @@ routes.get("/getAppointmentById", authController.getAppointmentById);
 // Get User Appointment Only
 routes.get("/getAppointmentByDoctorId", authController.getAppointmentByDoctorId);
 
-// Get Appointment by Doctor Id
-routes.get("/getAppointmentByDoctorId", authController.getAppointmentByDoctorId);
-
 module.exports = routes;
